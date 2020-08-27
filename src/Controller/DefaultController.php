0d65b1aaccb44c2893869d42b57cc9d370fdc44f
mathurin petit <mathurin.petit@gmail.com>
2020-08-27 13:20:54 +0200

    ajout composer

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,7 +12,44 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
+      $telephone = $this->getParameter('app.telephone');
+      $email = $this->getParameter('app.email');
+      $facebook = $this->getParameter('app.facebook');
+      $instagram = $this->getParameter('app.instagram');
+      $ytid = $this->getParameter('app.ytid');
+      // if($this->isMobile()){
+      //   return $this->redirectToRoute('index_mobile');
+      // }
+      return $this->render('default/index.html.twig',array('email' => $email, 'facebook' => $facebook, 'instagram' => $instagram, 'telephone' => $telephone, 'ytid' => $ytid));
+    }
+
+    /**
+     * @Route("/mobile")
+     */
+    public function indexMobileAction()
+    {
+      if(!$this->isMobile()){
+        return $this->redirectToRoute('index');
+      }
+
+      return $this->render('default/metacasquette_mobile.html.twig');
+    }
+
+    /**
+     * @Route("/path")
+     */
+    public function pathAction()
+    {
+      if($this->isMobile()){
+        return $this->redirectToRoute('index_mobile');
+      }
 
       return $this->render('default/metacasquette.html.twig');
     }
+
+    public function isMobile(){
+    $device = $this->get('mobile_detect.mobile_detector');
+
+    return $device->isMobile() || $device->isTablet();
+  }
 }
