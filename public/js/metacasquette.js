859a8836cb63ba50b3eb6e9b1adf0450e6fbe9c6
Mathurin Petit <mathurin.petit@gmail.com>
2022-07-02 10:58:52 +0200

    chargement au fur et a mesure

--- public/js/metacasquette.js
+++ public/js/metacasquette.js
@@ -3,6 +3,7 @@ $(document).ready(init);
 const rotates360 = {};
 const blocked = {};
 const deltaImg = {};
+const initRotate = {};
 var activePressed = null;
 var mobile_test = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
 
@@ -16,82 +17,14 @@ function init() {
 			}
 			cpt++;
 		});
-		initScrollEvent();
+		initRotateElements();
 	});
 
-
-	$(".rotate360").each(function(){
-
-		var id = $(this).attr("id");
-		var name = $(this).data("name");
-		var numberPictures = $(this).data("number");
-		var rotate = new ProductViewer({
-				element: 		document.getElementById(id),
-				imagePath: 		"../img/casquettes/"+name+"/product",
-				filePrefix: 	'',
-				fileExtension: '.jpg',
-				numberOfImages: numberPictures
-			});
-			rotates360[id] = rotate;
-			blocked[id] = false;
-	});
-
-
-
-
-	if(!mobile_test){
-
-		$(".rotate360").each(function(){
-				var id = $(this).attr("id");
-				deltaImg[id] = 0;
-				rotates360[id].on('press', () => {
-					deltaImg[id] = 0;
-					activePressed = id;
-				});
-
-				rotates360[id].on('delta', ({x, numberOfImages, offsetIndex}) => {
-					deltaImg[id] = offsetIndex;
-				})
-
-				rotates360[id].on('release', ({index, image}) => {
-					if (deltaImg[id] == 0 && id == activePressed) {
-						$(this).siblings("a").click();
-						activePressed = null;
-					}
-				});
-		});
-
-		$(".img_not_360").on('click',function(){
-			$(this).siblings("a").click();
-		});
-
-		$('.overlay-mobile').each(function(){
-			$(this).remove();
-		});
-		$('.img-mobile').each(function(){
-			$(this).remove();
-		});
-	}else{
-		$(".rotate360mobile").each(function(){
-			var id = $(this).attr("id");
-			var name = $(this).data("name");
-			var numberPictures = $(this).data("number");
-			var rotateMobile = new ProductViewer({
-					element: 		document.getElementById(id),
-					imagePath: 		"../img/casquettes/"+name+"/product",
-					filePrefix: 	'',
-					fileExtension: '.jpg',
-					numberOfImages: numberPictures
-				});
-			rotateMobile.once('loaded', () => $('#'+id+' .product-viewer__image').addClass("img-popup-mobile"));
-			rotateMobile.animate360(4000);
-		});
-	}
-
-	initScrollEvent();
+	initRotateElements();
 }
 
-function initScrollEvent(){
+function initRotateElements(){
+
 	$(window).on('scroll', function(){
 		$('.wrapper:not(.hidden)').children(".rotate360").each(function(){
 				var id = $(this).attr("id");
@@ -101,6 +34,75 @@ function initScrollEvent(){
 				}
 		});
 	});
+
+
+		$('.wrapper:not(.hidden)').children(".rotate360").each(function(){
+
+			var id = $(this).attr("id");
+			if(!initRotate[id]){
+				initRotate[id] = true;
+				var name = $(this).data("name");
+				var numberPictures = $(this).data("number");
+				var rotate = new ProductViewer({
+						element: 		document.getElementById(id),
+						imagePath: 		"../img/casquettes/"+name+"/product",
+						filePrefix: 	'',
+						fileExtension: '.jpg',
+						numberOfImages: numberPictures
+					});
+					rotates360[id] = rotate;
+					blocked[id] = false;
+				}
+		});
+		if(!mobile_test){
+
+			$('.wrapper:not(.hidden)').children(".rotate360").each(function(){
+					var id = $(this).attr("id");
+					deltaImg[id] = 0;
+					rotates360[id].on('press', () => {
+						deltaImg[id] = 0;
+						activePressed = id;
+					});
+
+					rotates360[id].on('delta', ({x, numberOfImages, offsetIndex}) => {
+						deltaImg[id] = offsetIndex;
+					})
+
+					rotates360[id].on('release', ({index, image}) => {
+						if (deltaImg[id] == 0 && id == activePressed) {
+							$(this).siblings("a").click();
+							activePressed = null;
+						}
+					});
+			});
+
+			$(".img_not_360").on('click',function(){
+				$(this).siblings("a").click();
+			});
+
+			$('.overlay-mobile').each(function(){
+				$(this).remove();
+			});
+			$('.img-mobile').each(function(){
+				$(this).remove();
+			});
+		}else{
+			$(".rotate360mobile").each(function(){
+				var id = $(this).attr("id");
+				var name = $(this).data("name");
+				var numberPictures = $(this).data("number");
+				var rotateMobile = new ProductViewer({
+						element: 		document.getElementById(id),
+						imagePath: 		"../img/casquettes/"+name+"/product",
+						filePrefix: 	'',
+						fileExtension: '.jpg',
+						numberOfImages: numberPictures
+					});
+				rotateMobile.once('loaded', () => $('#'+id+' .product-viewer__image').addClass("img-popup-mobile"));
+				rotateMobile.animate360(4000);
+			});
+		}
+
 }
 
 
