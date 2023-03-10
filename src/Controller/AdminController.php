90a7ece39b7b973c91978b8212da52f6cdd34c23
Mathurin Petit <mathurin.petit@gmail.com>
2023-03-10 12:37:44 +0100

    ajout et entretien des casquettes

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -35,27 +35,27 @@ class AdminController extends Controller
 
 
     /**
-     * @Route("/admin/list/{fileName}")
+     * @Route("/admin/list")
      */
-    public function adminListAction($fileName)
+    public function adminListAction()
     {
       $telephone = $this->getParameter('app.telephone');
       $email = $this->getParameter('app.email');
       $facebook = $this->getParameter('app.facebook');
       $instagram = $this->getParameter('app.instagram');
       $ytid = $this->getParameter('app.ytid');
+      $pathFiles = $this->getParameter('app.pathFiles');
 
 
       // instantiation, when using it inside the Symfony framework
       $serializer = $this->container->get('serializer');
 
 
-      $pathFiles = $this->getParameter('app.pathFiles');
-      if(!file_exists($pathFiles.'/'.$fileName.'.csv')){
+      if(!file_exists($pathFiles.'/liste.csv')){
         return $this->redirect('/');
       }
 
-      $metacasquettes = $serializer->decode(file_get_contents($pathFiles.'/'.$fileName.'.csv'), 'csv', [CsvEncoder::DELIMITER_KEY => ';']);
+      $metacasquettes = $serializer->decode(file_get_contents($pathFiles.'/liste.csv'), 'csv', [CsvEncoder::DELIMITER_KEY => ';']);
       foreach ($metacasquettes as $key => $metacasquette) {
         $id_casquette = sprintf("%04d", $metacasquette["Numero"]);
         $is_dir = is_dir("img/casquettes/".$id_casquette);
@@ -86,16 +86,49 @@ class AdminController extends Controller
 
       return $this->render('admin/adminList.html.twig',
               array('email' => $email, 'facebook' => $facebook, 'instagram' => $instagram, 'telephone' => $telephone,
-               'ytid' => $ytid, 'metacasquettes' => $metacasquettes, 'fileName' => $fileName));
+               'ytid' => $ytid, 'metacasquettes' => $metacasquettes));
 
     }
 
 
+    /**
+     * @Route("/admin/add")
+     */
+    public function adminAddAction(Request $request)
+    {
+
+      $pathFiles = $this->getParameter('app.pathFiles');
+      $serializer = $this->container->get('serializer');
+      $metacasquettes = $serializer->decode(file_get_contents($pathFiles.'/liste.csv'), 'csv', [CsvEncoder::DELIMITER_KEY => ';']);
+
+      $lastmetacasquette = end($metacasquettes);
+
+      $newId = intval(str_replace('#','',$lastmetacasquette["Numero"]))+1;
+
+      $lastmetacasquette["Numero"] = "#".$newId;
+      $lastmetacasquette["Description"] = "";
+      $lastmetacasquette["Matieres"] = "";
+      $lastmetacasquette["Taille"] = "";
+      $lastmetacasquette["Code"] = "";
+      $lastmetacasquette["Disponibilite"] = "";
+      $lastmetacasquette["Proprietaire"] = "";
+      $lastmetacasquette["NumeroLogo"] = "";
+      $lastmetacasquette["Cache"] = "";
+      $lastmetacasquette["Etat"] = "";
+      $lastmetacasquette["Instagram"] = "Non";
+
+      $metacasquettes[] = $lastmetacasquette;
+
+      file_put_contents($pathFiles.'/liste.csv',$serializer->encode($metacasquettes, 'csv', [CsvEncoder::DELIMITER_KEY => ';']));
+
+      return $this->redirect($this->generateUrl('adminEdit', array('id' => $newId)));
+    }
+
 
     /**
-     * @Route("/admin/edit/{fileName}/{id}")
+     * @Route("/admin/edit/{id}")
      */
-    public function adminEditAction(Request $request, $fileName,$id)
+    public function adminEditAction(Request $request,$id)
     {
       $telephone = $this->getParameter('app.telephone');
       $email = $this->getParameter('app.email');
@@ -110,22 +143,33 @@ class AdminController extends Controller
       // encoding contents in CSV format
 
       $pathFiles = $this->getParameter('app.pathFiles');
-      if(!file_exists($pathFiles.'/'.$fileName.'.csv')){
+      if(!file_exists($pathFiles.'/liste.csv')){
         return $this->redirect('/');
       }
 
 
 
-      $metacasquettes = $serializer->decode(file_get_contents($pathFiles.'/'.$fileName.'.csv'), 'csv', [CsvEncoder::DELIMITER_KEY => ';']);
+      $metacasquettes = $serializer->decode(file_get_contents($pathFiles.'/liste.csv'), 'csv', [CsvEncoder::DELIMITER_KEY => ';']);
       $form = null;
       foreach ($metacasquettes as $key => $row) {
         $id_casquette = sprintf("%04d", str_replace('#','',$row["Numero"]));
         if($id_casquette == $id){
           $metacasquette = $row;
+          $NumeroLogoExists = $metacasquette['NumeroLogo'];
           $form = $this->createFormBuilder($metacasquette)
             ->add('Numero', TextType::class)
             ->add('Code', TextType::class)
+            ->add('Annee', TextType::class)
             ->add('Description', TextareaType::class)
+            ->add('Matieres', TextareaType::class)
+            ->add('Taille', TextType::class)
+            ->add('Disponibilite', TextType::class)
+            ->add('Proprietaire', TextType::class)
+            ->add('NumeroLogo', TextType::class,array('disabled' => $metacasquette['NumeroLogo']))
+            ->add('Cache', TextType::class)
+            ->add('Etat', TextType::class)
+            ->add('Instagram', TextType::class)
+
             ->add('Enregistrer', SubmitType::class, ['label' => 'Enregistrer'])
             ->getForm();
           break;
@@ -143,8 +187,8 @@ class AdminController extends Controller
             }
           }
           //var_dump($serializer->encode($metacasquettes, 'csv', [CsvEncoder::DELIMITER_KEY => ';'])); exit;
-          file_put_contents($pathFiles.'/'.$fileName.'.csv',$serializer->encode($metacasquettes, 'csv', [CsvEncoder::DELIMITER_KEY => ';']));
-          return $this->redirect($this->generateUrl('adminList',array('fileName' => 'liste')));
+          file_put_contents($pathFiles.'/liste.csv',$serializer->encode($metacasquettes, 'csv', [CsvEncoder::DELIMITER_KEY => ';']));
+          return $this->redirect($this->generateUrl('adminList'));
 
       }
 
